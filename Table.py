17e552f48a4102e1d7bad2ed815c79595fda9fd2
Som-Hatalkar <somhatalkar11@gmail.com>
2024-06-20 12:07:14 +0530

    A python program for printing 2 to 40 Tables

---
+++ Table.py
@@ -0,0 +1,21 @@
+# print("2 x 1 = ", 2*1)
+# print("2 x 2 = ", 2*2)
+# print("2 x 3 = ", 2*3)
+# print("2 x 4 = ", 2*4)
+# print("2 x 5 = ", 2*5)
+# print("2 x 6 = ", 2*6)
+# print("2 x 7 = ", 2*7)
+# print("2 x 8 = ", 2*8)
+# print("2 x 9 = ", 2*9)
+# print("2 x 10 = ", 2*10)
+    
+
+# x= [1,2,3,4,5,6,7,8,9,10]
+# for i in x :
+#     f"print ("2 x {i} =",2*{i})"
+
+
+for i in range(2,41):
+    for j in range(1, 11):
+        print(i, 'x', j, '=', i*j)
+    print("")
